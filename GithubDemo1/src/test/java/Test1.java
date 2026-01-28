b01560d2a901d96347c0141a27c7a3681b866ba7
Mohan17901 <deviprasadareti@gmail.com>
2026-01-28 13:11:57 +0530

    First Commit ......

---
+++ GithubDemo1/src/test/java/Test1.java
@@ -0,0 +1,20 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String hungry="yes";
+		
+		if(hungry.equals("yes")) {
+			System.out.println("eat something ........");
+		}
+		else {
+		System.out.println("Do work.........");
+		//vinay
+		System.out.println("eat later .......");
+		}
+	
+
+	}
+
+}
